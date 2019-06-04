654ca62a07a2dfa6c3d611d8c128d43afaf5c2b3
Student <kaczanowskamonika@gmail.com>
2019-06-04 22:31:04 +0100

    Min and Max balance added, raising an error if there's not enough money on oyster

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,15 +1,35 @@
 class Oystercard
 
-  attr_reader :balance
+   attr_reader :balance
+
   MAX_BALANCE = 90
+  MIN_BALANCE = 1
+
+
     def initialize
       @balance = 0
+      @in_journey = false
     end
 
     def top_up(value)
 
-      raise "Maximum balance is exceeded!" if @balance + value > 90
+      raise "Maximum balance is exceeded!" if @balance + value > MAX_BALANCE
+
       @balance += value
+
+    end
+
+    def deduct(value)
+    end
+
+    def in_journey?
+      @in_journey
+    end
+
+    def touch_in
+      raise "Errrr, no money" if @balance < MIN_BALANCE
+
+      @in_journey = true
 
     end
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -2,20 +2,44 @@ require 'oystercard'
 
 describe Oystercard do
   it 'has a balance of zero' do
-    oystercard = Oystercard.new
-    expect(oystercard.balance).to eq 0
+    # oystercard = Oystercard.new same as subject in line 6
+    expect(subject.balance).to eq 0
   end
 
-describe '#top_up' do
+  describe '#top_up' do
 
-  it{ is_expected.to respond_to(:top_up).with(1).argument}
-end
+    it{ is_expected.to respond_to(:top_up).with(1).argument}
+
+  end
+
   it 'can top up the balance' do
     expect { subject.top_up 1 }.to change{subject.balance}.by 1
-end
-  it 'lets the customer top up up to £90' do
-    max_balance = Oystercard::MAX_BALANCE
-    subject.top_up max_balance #this line sets up a block - max is 90 and I can't go above that!
-    expect {subject.top_up 1}.to raise_error "Maximum balance is exceeded!"
   end
+    it 'lets the customer top up up to £90' do
+      max_balance = Oystercard::MAX_BALANCE
+      subject.top_up max_balance #this line sets up a block - max is 90 and I can't go above that!
+      expect {subject.top_up 1}.to raise_error "Maximum balance is exceeded!"
+    end
+
+    describe '#deduct' do
+      it{ is_expected.to respond_to(:deduct).with(1).argument}
+    end
+
+    ####
+   describe '#in_journey?'do
+      it 'is initially not in journey' do
+        expect(subject).not_to be_in_journey
+      end
+    end
+      describe "#touch_in" do
+        it 'can check in' do
+          subject.top_up(5)
+          expect(subject.touch_in).to be true
+        end
+      end
+       describe '#touch_in' do
+         it 'allows the customer to start the journey if minimal amount is £1' do
+           expect { subject.touch_in }.to raise_error("Errrr, no money")
+         end
+   end
 end
